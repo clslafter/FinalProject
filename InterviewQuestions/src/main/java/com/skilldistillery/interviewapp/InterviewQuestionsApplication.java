8e1943f7930351ed51ed4714015c984acf1bfe62
clslafter <clslafter@att.net>
2022-11-22 11:51:15 -0600

    Create boot project

---
+++ InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/InterviewQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.interviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewQuestionsApplication.class, args);
+	}
+
+}
