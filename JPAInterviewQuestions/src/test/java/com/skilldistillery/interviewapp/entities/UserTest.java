7547d1fc886a8accb9197e97958a364a32bb17d2
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-11-22 11:57:03 -0700

    stub user, test hibernate

--- JPAInterviewQuestions/src/test/java/com/skilldistillery/interviewapp/entities/UserTest.java
+++ JPAInterviewQuestions/src/test/java/com/skilldistillery/interviewapp/entities/UserTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.interviewapp.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,26 +13,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAInterviewQuestions");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_entity_mapping() {
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
 	}
 
 }
