9b29ed60786c60c3ae766f128e23b45e3494cd02
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-11-26 10:58:48 -0700

    added models, user, address, answerrating, jobopening,

---
+++ ngInterview/src/app/models/address.ts
@@ -0,0 +1,30 @@
+import { ComponentFactory } from "@angular/core";
+import { Company } from "./company";
+import { JobOpening } from "./job-opening";
+
+export class Address {
+  id: number;
+  street: string;
+  street2: string;
+  city: string;
+  state: string;
+  zip: string;
+  enabled: boolean;
+  company: Company;
+  job: JobOpening;
+  constructor(
+    id: number = 0, street: string = '', street2: string = '',
+    city: string = '', state: string = '', zip: string = '',
+    enabled: boolean = true, company: Company, job: JobOpening,
+  ){
+    this.id = id;
+    this.street = street;
+    this.street2 = street2;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.enabled = enabled;
+    this.company = company;
+    this.job = job;
+  }
+}
