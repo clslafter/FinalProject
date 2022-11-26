6d2f3c49b5eca4c1d7e574b66c91df133ac1dacf
clslafter <clslafter@att.net>
2022-11-26 11:58:51 -0600

    created and stubbed out navigation, login, welcome, and register components

--- ngInterview/src/app/app-routing.module.ts
+++ ngInterview/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
---
+++ ngInterview/src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginUser = new User();
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(user: User) {
+    console.log('Logging user:');
+    console.log(user);
+  //   this.auth.login(user.username, user.password).subscribe({
+  //     next: (loggedInUser) => {
+  //           this.router.navigateByUrl('/todo');
+  //         },
+  //     error: (fail) => {
+  //       console.error('LoginComponent.login(): Error logging in account');
+  //       console.error(fail);
+  //     }
+  //   });
+  }
+
+}
