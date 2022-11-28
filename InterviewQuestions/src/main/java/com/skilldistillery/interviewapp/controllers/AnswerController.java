7d936ff3f2bb60b3472331d5ae41a63d31c54689
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-11-28 11:52:28 -0700

    added stuff to answer Controller

--- InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/controllers/AnswerController.java
+++ InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/controllers/AnswerController.java
@@ -14,33 +14,48 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.interviewapp.entities.Answer;
+import com.skilldistillery.interviewapp.entities.Question;
 import com.skilldistillery.interviewapp.services.AnswerService;
+import com.skilldistillery.interviewapp.services.QuestionService;
 
 @RestController
-@CrossOrigin({"*", "http://localhost/"})
+@CrossOrigin({ "*", "http://localhost/" })
 public class AnswerController {
-	
+
 	@Autowired
 	private AnswerService answerService;
-	
+
+	@Autowired
+	private QuestionService questionService;
+
 	@GetMapping("answers")
-	public List<Answer> index(HttpServletRequest req, HttpServletResponse res){
+	public List<Answer> index(HttpServletRequest req, HttpServletResponse res) {
 		return answerService.answerList();
 	}
-	
+
 	@DeleteMapping("api/answers/{aid}")
-	public void destroy(HttpServletRequest req, HttpServletResponse res, @PathVariable int aid, Principal principal) {
+	public void destroy(HttpServletRequest req, HttpServletResponse res, 
+			@PathVariable int aid, Principal principal) {
 		try {
-			if(answerService.delete(principal.getName(), aid)) {
+			if (answerService.delete(principal.getName(), aid)) {
 				res.setStatus(204);
 			} else {
 				res.setStatus(404);
 			}
 		} catch (Exception e) {
-			
 			e.printStackTrace();
 			res.setStatus(400);
 		}
 	}
 
+	@GetMapping("api/answers/{aid}/questions/{qid}")
+	public List<Answer> questionsAnswer(HttpServletRequest req, HttpServletResponse res, @PathVariable int aid,
+			@PathVariable int qid, Principal principal) {
+		Question question = questionService.show(qid);
+		List<Answer> answers = answerService.questionAnswers(aid);
+		if (question == null | answers == null) {
+			res.setStatus(404);
+		}
+		return answers;
+	}
 }
