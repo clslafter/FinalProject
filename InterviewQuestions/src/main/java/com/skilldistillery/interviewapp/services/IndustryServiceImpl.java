1e4a20f82ed58e61addcf2c9196efda0f79edac5
Josh Ingram <joshua.n.ingram@gmail.com>
2022-11-29 00:04:28 -0700

    created Industry Service Impl

---
+++ InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/services/IndustryServiceImpl.java
@@ -0,0 +1,22 @@
+package com.skilldistillery.interviewapp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.interviewapp.entities.Industry;
+import com.skilldistillery.interviewapp.repositories.IndustryRepository;
+
+@Service
+public class IndustryServiceImpl implements IndustryService {
+	
+	@Autowired
+	private IndustryRepository industryRepo;
+
+	@Override
+	public List<Industry> industryList() {
+		return industryRepo.findAll();
+	}
+
+}
