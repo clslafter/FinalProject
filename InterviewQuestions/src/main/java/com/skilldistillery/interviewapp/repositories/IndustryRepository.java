872218c1c802ffcf9251a539f905c90e9be3a8bd
Josh Ingram <joshua.n.ingram@gmail.com>
2022-11-29 00:00:44 -0700

    created Industry Repo

---
+++ InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/repositories/IndustryRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.interviewapp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.interviewapp.entities.Industry;
+
+public interface IndustryRepository extends JpaRepository<Industry, Integer>{
+
+}
