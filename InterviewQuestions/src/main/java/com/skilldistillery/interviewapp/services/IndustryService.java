0718e9c021a604a9a963bda0c9ff2da2a6a3bb2c
Josh Ingram <joshua.n.ingram@gmail.com>
2022-11-29 00:02:22 -0700

    created industry service

---
+++ InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/services/IndustryService.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.interviewapp.services;
+
+import java.util.List;
+
+import com.skilldistillery.interviewapp.entities.Industry;
+
+public interface IndustryService {
+
+	List<Industry> industryList();
+
+}
