8a0c476a9a43063be71c4c6df0698ac811480e70
clslafter <clslafter@att.net>
2022-11-30 16:36:57 -0600

    created enabled Question pipe

---
+++ ngInterview/src/app/pipes/enabled-question.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { EnabledQuestionPipe } from './enabled-question.pipe';
+
+describe('EnabledQuestionPipe', () => {
+  it('create an instance', () => {
+    const pipe = new EnabledQuestionPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
