f5d6010a602f9cf00a787204e8289c3ad87959d9
Josh Ingram <joshua.n.ingram@gmail.com>
2022-11-30 13:02:49 -0700

    company detail component shows company detail, but not yet their list of questions

--- ngInterview/src/app/components/company-detail/company-detail.component.ts
+++ ngInterview/src/app/components/company-detail/company-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Company } from 'src/app/models/company';
+import { CompanyService } from 'src/app/services/company.service';
 
 @Component({
   selector: 'app-company-detail',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyDetailComponent implements OnInit {
 
-  constructor() { }
+  selected: Company | null = null;
+
+  constructor(private companyService: CompanyService,private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.loadPage();
   }
+
+  loadPage(){
+    let routeId = this.route.snapshot.paramMap.get('id');
+    console.log(routeId);
+    if (!this.selected && routeId) {
+      let companyId = Number.parseInt(routeId);
+      if(isNaN(companyId)) {
+        this.router.navigateByUrl('invalidId');
+      } else {
+        this.companyService.show(companyId).subscribe({
+          next: (data: any) => {
+            this.selected = data;
+          },
+          error: (fail: any) => {
+            console.error('CompanyDetailComponent.ngOnInit: company not found');
+            this.router.navigateByUrl('companyNotFound'); //doesn't exist
+          }
+        });
+      }
+    }
+    }
 
 }
