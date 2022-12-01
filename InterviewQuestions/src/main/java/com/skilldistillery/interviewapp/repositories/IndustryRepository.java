6daa08e4e3c6bf2ed37cec7e5e1fbdfc1664def2
clslafter <clslafter@att.net>
2022-12-01 14:30:35 -0600

    added backend industry add and remove to company service impl

--- InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/repositories/IndustryRepository.java
+++ InterviewQuestions/src/main/java/com/skilldistillery/interviewapp/repositories/IndustryRepository.java
@@ -2,10 +2,13 @@ package com.skilldistillery.interviewapp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.skilldistillery.interviewapp.entities.Industry;
 
 public interface IndustryRepository extends JpaRepository<Industry, Integer>{
 	
 	Industry findById (int idd);
+	
+	Industry queryById(int indId);
 
 }
